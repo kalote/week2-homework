2640cd62bfbb79239bdb8b3f90b6e9c24e4c1fb8
Jonathan Macke <jonathan.macke@yahoo.fr>
2022-10-02 23:21:55 +0200

    add voting task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { config as LoadEnv } from 'dotenv';
+import "./tasks/Voting";
 
 LoadEnv();
 
